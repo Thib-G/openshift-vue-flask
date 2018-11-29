67f3c85f610a6c04078858cf542b33d45e69e98c
Thib <thibaut.g@protonmail.com>
2018-11-29 08:25:18 +0100

    Change port

--- api/build.sh
+++ api/build.sh
@@ -2,7 +2,7 @@ export image_name=flask-s2i
 # export base_image=centos/python-36-centos7
 export base_image=registry.access.redhat.com/rhscl/python-36-rhel7
 s2i build . $base_image $image_name-image
-docker run --rm -ti --name $image_name-container -p 5000:8080 $image_name-image
+docker run --rm -ti --name $image_name-container -p 5000:5000 $image_name-image
 docker rmi $image_name-image
 # docker rmi $base_image
 docker image prune --force
