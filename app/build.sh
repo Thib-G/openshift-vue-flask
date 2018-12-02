b77e1d57236282321f03dab4e47a125d73058767
Thib <thibaut.g@protonmail.com>
2018-12-02 18:17:21 +0100

    Use RHOAR image

--- app/build.sh
+++ app/build.sh
@@ -1,7 +1,7 @@
 export image_name=nginx-s2i
 # export base_image=centos/python-36-centos7
-export base_image=registry.access.redhat.com/rhscl/httpd-24-rhel7
-s2i build . $base_image $image_name-image
+export base_image=registry.access.redhat.com/rhoar-nodejs-tech-preview/rhoar-nodejs-10-webapp
+s2i build . $base_image $image_name-image OUTPUT_DIR=dist
 docker run --rm -ti --name $image_name-container -p 8080:8080 $image_name-image
 docker rmi $image_name-image
 # docker rmi $base_image
