50ed0175647740dac59625dd1b0600d8e8880b58
Thib <thibaut.g@protonmail.com>
2018-12-03 11:23:56 +0100

    Change built.sh

--- app/build.sh
+++ app/build.sh
@@ -1,4 +1,4 @@
-export image_name=nginx-s2i
+export image_name=webapp-s2i
 # export base_image=centos/python-36-centos7
 export base_image=registry.access.redhat.com/rhoar-nodejs-tech-preview/rhoar-nodejs-10-webapp
 s2i build . $base_image $image_name-image
